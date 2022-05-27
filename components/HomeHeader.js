b33ce88f069b77080d9304247cbd395ea9f34574
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-05-27 14:45:45 +0100

    add verify badge

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, Image, TextInput } from "react-native";
+import { COLORS, FONTS, SHADOWS, SIZES, assets } from "../constants";
 
 export const HomeHeader = () => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.font,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alight: "center",
+        }}
+      >
+        <Image
+          source={assets.logo}
+          resizeMode="contain"
+          style={{ width: 90, height: 25 }}
+        />
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              width: 15,
+              height: 15,
+              position: "absolute",
+              right: 0,
+              bottom: 0,
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 };
