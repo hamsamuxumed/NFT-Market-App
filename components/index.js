0b5ac7c38d26be4a23988d15520bcd9d0391afa9
“hamsamuxumed” <hamsamuxumed3@gmail.com>
2022-05-27 11:22:00 +0100

    project files

---
+++ components/index.js
@@ -0,0 +1,5 @@
+//export { NFTCard } from "./NFTCard";
+export { HomeHeader } from "./HomeHeader";
+export { FocusedStatusBar } from "./FocusedStatusBar";
+export { LikeButton, RectButton } from "./Buttons";
+//export { SubInfo, NFTTitle, ETHPrice } from "./SubInfo";
